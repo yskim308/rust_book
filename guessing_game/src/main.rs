4244ff6e00aeaa2a12ca52ca17225869bd71753a
Young Seo Kim <youngseokim7@gmail.com>
2025-11-16 17:00:10 +0900

    intial commit

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("guess the number hoe");
+    println!("input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("failed to read line");
+
+    println!("you guessed: {guess}");
 }
