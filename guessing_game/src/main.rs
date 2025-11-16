2d8d28f16ba34b6dd864c6d36d6789687c26761c
Young Seo Kim <youngseokim7@gmail.com>
2025-11-16 17:14:36 +0900

    loop

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,19 +8,21 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("the secret number is {secret_number}");
-    let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("failed to read line");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("enter a nubmer");
+        let guess: u32 = guess.trim().parse().expect("enter a nubmer");
 
-    println!("you guessed: {guess}");
+        println!("you guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small"),
-        Ordering::Greater => println!("too big"),
-        Ordering::Equal => println!("you win !"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => println!("you win !"),
+        }
     }
 }
