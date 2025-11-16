b236e97f3ed01dc6d035dc05c8fa4f614f7ac822
Young Seo Kim <youngseokim7@gmail.com>
2025-11-16 17:05:41 +0900

    getting rand number from crate import

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,13 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
     println!("guess the number hoe");
     println!("input your guess");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("the secret number is {secret_number}");
     let mut guess = String::new();
 
     io::stdin()
