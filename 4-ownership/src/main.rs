24ca62bf6dbd11e52a01142390d0f7b40b359dc7
Young Seo Kim <youngseokim7@gmail.com>
2025-11-19 11:26:59 +0900

    clone example

--- 4-ownership/src/main.rs
+++ 4-ownership/src/main.rs
@@ -3,4 +3,9 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1;
     println!("{s2}");
+
+    // to clone, use clone method on strings, expensive
+    let s3 = String::from("clone");
+    let s4 = s3.clone();
+    println!("{s4}");
 }
