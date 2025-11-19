df2aaf0337adf3c50cedd7fc151a4c83f0ea745d
Young Seo Kim <youngseokim7@gmail.com>
2025-11-19 11:34:56 +0900

    complete ownership give example

--- 4-ownership/src/main.rs
+++ 4-ownership/src/main.rs
@@ -9,12 +9,21 @@ fn main() {
     let s4 = s3.clone();
     println!("{s4}");
 
+    // ownership of s2 moved to function, and goes out of scope
     takes_ownership(s2);
 
-    // println!("{s2}");
+    // owernship given by return value
+    let given = gives_ownership();
+    println!("{given}");
 }
 
 fn takes_ownership(some_string: String) {
     // ownership of some_string transferred
     println!("'{some_string}' ownership transferred");
 } // goes out of scope
+
+// ownership given by function return value
+fn gives_ownership() -> String {
+    let some_string = String::from("yours now");
+    some_string
+}
