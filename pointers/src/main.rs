7813f08e00505932974a3793b80d466db3ef848b
Young Seo Kim <youngseokim7@gmail.com>
2025-11-22 18:32:47 +0900

    example of smart pointer and drop

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,5 +1,6 @@
 use std::ops::Deref;
 
+// ----- smart pointer implementation -----
 struct MyBox<T>(T);
 
 impl<T> Deref for MyBox<T> {
@@ -15,9 +16,31 @@ impl<T> MyBox<T> {
     }
 }
 
+// ----- implementation to demonstrate drop ------
+#[derive(Debug)]
+struct CustomSmartPointer<T: std::fmt::Debug> {
+    data: T,
+}
+
+impl<T: std::fmt::Debug> Drop for CustomSmartPointer<T> {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data: {:?}", self.data);
+    }
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
     assert_eq!(5, x);
     assert_eq!(5, *y);
+
+    println!("smart pointer with Hello World created");
+    let c = CustomSmartPointer {
+        data: String::from("hello world"),
+    };
+    drop(c);
+    println!("smart pointer with hello world dropped before end of program");
+
+    println!("smart poitner with integer 5 created");
+    let _d = CustomSmartPointer { data: 5 };
 }
