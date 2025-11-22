5d860fe82374cadb8f828b317f943cefd2750c3e
Young Seo Kim <youngseokim7@gmail.com>
2025-11-22 18:11:20 +0900

    Implement dereference for custom mybox type

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -1,4 +1,13 @@
+use std::ops::Deref
+
 struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 impl<T> MyBox<T> {
     fn new(x: T) -> MyBox<T> {
