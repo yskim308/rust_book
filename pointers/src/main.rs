a2eafb6070f65e5706c18458c2c7bf75245a3e91
Young Seo Kim <youngseokim7@gmail.com>
2025-11-22 19:12:16 +0900

    notes for Rc and RefCell

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -28,12 +28,24 @@ impl<T: std::fmt::Debug> Drop for CustomSmartPointer<T> {
     }
 }
 
+// ---- implementation to demonstrate Rc and RefCel -----
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
 fn main() {
+    // ---- box examples
     let x = 5;
     let y = MyBox::new(x);
     assert_eq!(5, x);
     assert_eq!(5, *y);
 
+    // ---- smart pointer with drop examples
     println!("smart pointer with Hello World created");
     let c = CustomSmartPointer {
         data: String::from("hello world"),
@@ -43,4 +55,27 @@ fn main() {
 
     println!("smart poitner with integer 5 created");
     let _d = CustomSmartPointer { data: 5 };
+
+    // --- examples for Rc and RefCell -----
+    let value = Rc::new(RefCell::new(5));
+
+    // a is a new countable reference to a list where
+    // value : countable reference to value (clone)
+    // list : Nil
+    let a = Rc::new(List::Cons(Rc::clone(&value), Rc::new(List::Nil)));
+
+    // b is a new list where
+    // value: mutable refcell of 3
+    // list: the list a (which can have multiple references)
+    let b = List::Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+
+    // same as b
+    let c = List::Cons(Rc::new(RefCell::new(5)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    // ssentially, every value in the list is a multi-referenced interior mutable value
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
 }
