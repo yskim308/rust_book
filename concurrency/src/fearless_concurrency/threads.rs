e3b95a4a92e8d82ad593139fe81b8e3629fadf0b
Young Seo Kim <youngseokim7@gmail.com>
2025-11-23 16:36:18 +0900

    modularity

---
+++ concurrency/src/fearless_concurrency/threads.rs
@@ -0,0 +1,6 @@
+use std::thread;
+use std::time::Duration;
+
+pub fn hello_world() {
+    println!("hello world")
+}
--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,3 +1,8 @@
+mod fearless_concurrency;
+
+use fearless_concurrency::threads;
+
 fn main() {
     println!("Hello, world!");
+    threads::hello_world();
 }
