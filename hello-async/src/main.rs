35ce5dbc7116ceac29c4dfd8b6ca6a5647687d38
Young Seo Kim <youngseokim7@gmail.com>
2025-11-23 18:09:20 +0900

    todo

--- hello-async/src/main.rs
+++ hello-async/src/main.rs
@@ -1,3 +1,15 @@
+use trpl::Html;
+
+async fn page_title(url: &str) -> Option<String> {
+    let response = trpl::get(url).await;
+    let response_text = response.text().await;
+    Html::parse(&response_text)
+        .select_first("title")
+        .map(|title| title.inner_html())
+}
+
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
     println!("Hello, world!");
 }
